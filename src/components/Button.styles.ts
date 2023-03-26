c9d4d678cc8082a27fbd3a553fc605b594d6f6c6
Varner Silveira Cardoso <varnersilveiracardoso@iMac-de-Varner.local>
2023-03-26 19:48:54 -0300

    first commit

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,25 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariant = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  ${(props) => {
+    return css`
+      background-color: ${buttonVariant[props.variant]};
+    `;
+  }}
+`;
